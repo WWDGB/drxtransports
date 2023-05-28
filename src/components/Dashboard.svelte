e1385d82052d29aac081f95ae8f9622675c65f43
David <davidgeorgebogdan@gmail.com>
2023-05-28 20:37:23 +0300

    Display the transports in Dashboard component

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -1,38 +1,68 @@
+<script>
+  import axios from "axios";
+  import { onMount } from 'svelte';
+
+  /**
+   * @type {any[]}
+   */
+  let transports = [];
+
+  /**
+   * @type {any[]}
+   */
+  let drivers = [];
+
+  /**
+   * @type {any[]}
+   */
+  let locations = [];
+
+  async function getTransports() {
+    try {
+      const response = await axios.get('http://localhost:8080/transport');
+      transports = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getDriversName() {
+    try {
+      const response = await axios.get('http://localhost:8080/driver');
+      drivers = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getLocationsAddress() {
+    try {
+      const response = await axios.get('http://localhost:8080/location');
+      locations = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  onMount(async () => {
+    await getTransports();
+    await getDriversName();
+    await getLocationsAddress();
+  })
+</script>
+
 <div class="container mx-auto px-4">
-    <h1 class="text-4xl font-bold mb-8">Most recent transports</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      <!-- Transport Card -->
-      <div class="bg-white rounded-md shadow-lg border border-gray-300 p-6">
-        <h2 class="text-lg font-bold mb-4">Transport #1</h2>
-        <p><span class="font-bold">Driver:</span> John Doe</p>
-        <p><span class="font-bold">Location:</span> New York</p>
-        <p><span class="font-bold">Status:</span> In Transit</p>
-        <p><span class="font-bold">Dispatch Date:</span> May 1, 2023</p>
-        <p><span class="font-bold">Delivery Date:</span> May 5, 2023</p>
-      </div>
-  
-      <!-- Transport Card -->
-      <div class="bg-white rounded-md shadow-lg border border-gray-300 p-6">
-        <h2 class="text-lg font-bold mb-4">Transport #2</h2>
-        <p><span class="font-bold">Driver:</span> Jane Smith</p>
-        <p><span class="font-bold">Location:</span> Los Angeles</p>
-        <p><span class="font-bold">Status:</span> Delayed</p>
-        <p><span class="font-bold">Dispatch Date:</span> May 3, 2023</p>
-        <p><span class="font-bold">Delivery Date:</span> May 10, 2023</p>
-      </div>
-  
-      <!-- Transport Card -->
-      <div class="bg-white rounded-md shadow-lg border border-gray-300 p-6">
-        <h2 class="text-lg font-bold mb-4">Transport #3</h2>
-        <p><span class="font-bold">Driver:</span> Bob Johnson</p>
-        <p><span class="font-bold">Location:</span> Chicago</p>
-        <p><span class="font-bold">Status:</span> Completed</p>
-        <p><span class="font-bold">Dispatch Date:</span> May 2, 2023</p>
-        <p><span class="font-bold">Delivery Date:</span> May 6, 2023</p>
-      </div>
-  
-      <!-- Add more Transport Cards as needed -->
-  
+  <h1 class="text-4xl font-bold mb-8">Most recent transports</h1>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    {#each transports as transport}
+    <div class="bg-white rounded-md shadow-lg border border-gray-300 p-6">
+      <h2 class="text-lg font-bold mb-4">Transport #{transport.transportId}</h2>
+      <p><span class="font-bold">Driver:</span> {drivers.find(d => d.driverId === transport.driverId)?.firstName} {drivers.find(d => d.driverId === transport.driverId)?.lastName}</p>
+      <p><span class="font-bold">Location:</span> {locations.find(l => l.locationId === transport.locationId)?.address}</p>
+      <p><span class="font-bold">Status:</span> {transport.status}</p>
+      <p><span class="font-bold">Dispatch Date:</span> {transport.dispatchDate}</p>
+      <p><span class="font-bold">Delivery Date:</span> {transport.deliveryDate}</p>
     </div>
+    {/each}
   </div>
-  
+</div>
