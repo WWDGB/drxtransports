db2a77d43963eae7935ca1926bef5c0795699fce
David <davidgeorgebogdan@gmail.com>
2023-05-28 20:15:21 +0300

    Implemented all the functionality for 'New Transport' page

--- src/routes/makereservation/+page.svelte
+++ src/routes/makereservation/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import axios from 'axios';
 	import {onMount} from 'svelte';
-	import { each } from 'svelte/internal';
 	import Navbar from '../../components/Navbar.svelte';
   import { Button } from 'flowbite-svelte';
 
@@ -21,6 +20,12 @@
    */
   let statuses = [];
   let status = '';
+
+  /**
+   * @type {any[]}
+   */
+  let gates = [];
+  let gateId = 0;
 
   let dispatchDate = '';
   let deliveryDate = '';
@@ -57,19 +62,50 @@
 
   async function addTransport() {
       try{
-        const response = await axios.post('http://localhost:8080/transport', {
+        const transportResponse = await axios.post('http://localhost:8080/transport', {
           driverId,
           locationId,
           status,
           dispatchDate,
           deliveryDate,
-          deliveryHour
         });
-        console.log(response.data);
+
+        const transportId = transportResponse.data; //Retrieve the transport_id from the response 
+
+        const programResponse = await axios.post('http://localhost:8080/program', {
+          gateId,
+          transportId,
+          deliveryHour
+        })
+        
+        console.log(transportId);
+        console.log(programResponse.data);
       }catch (error){
         console.error(error);
       }
-    }
+  }
+
+  /**
+	 * 
+	 * @param {any[]} locationId
+	 */
+  async function getGates(locationId) {
+  try {
+    const response = await axios.get(`http://localhost:8080/gate/location/${locationId}`);
+    gates = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+  $: {
+  if (locationId !== 0) {
+    // @ts-ignore
+    getGates(locationId);
+  }
+}
+
+
 
 	onMount(() => {
 		getDrivers();
@@ -96,6 +132,15 @@
           <select class="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" id = "location"bind:value={locationId}>
             {#each locations as location}
               <option value={location.locationId}>{location.address}</option>
+            {/each}
+          </select>
+        </div>
+
+        <div class="mb-4">
+          <label for="gate" class="block text-gray-700 font-bold mb-2">Gate</label>
+          <select class="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" id="gate" bind:value={gateId}>
+            {#each gates as gate}
+              <option value={gate.gateId}>{gate.gateId}</option>
             {/each}
           </select>
         </div>
